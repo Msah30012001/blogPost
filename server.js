8856100bdd598744e930bcc65debdbbf95cfb625
Manish Shambhoo Sah <56181606+Msah30012001@users.noreply.github.com>
2023-11-28 20:09:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,19 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-
 const PORT = 5000;
 
 // mandatory middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:5000"],
-    credentials: true,
-  })
-);
-
-
-app.use(cookieParser());
+app.use(cors());
 
 //import a api routes
-
-// const admin_route = require("./routes/adminRoute");
 const post_route = require("./routes/blogPostRoute");
 //middleware for routing
 
-// app.use("/api/user", admin_route);
 app.use("/api/blog-post",post_route);
+
 // error handeling middleware
 
 app.use((error, req, res, next) => {
